4a7d9a2a53377ac7bfd4e2819a924e91aaa127fc
Divya Agrawal <divyaagrawal0747@gmail.com>
2024-08-08 16:27:31 +0530

    Refactor Dashboard components

--- client/src/components/Dashboard/DashCalendarCard.jsx
+++ client/src/components/Dashboard/DashCalendarCard.jsx
@@ -7,17 +7,14 @@ function DashCalendarCard({title, number}) {
         <div className="absolute rounded-md border bg-white h-8 w-8 -top-2 flex justify-center items-center">
             <img src={UserIcon} alt="" />
         </div>
-        <div className="grid grid-cols-2">
-            <div>
-                {/* Empty Div */}
-            </div>
+        <div className="flex justify-end">
             <div className='flex flex-col gap-2 text-right'>
-                <span className='text-[10px]'>{title || "Sample Text"}</span>
+                <span className='text-[12px]'>{title || "Sample Text"}</span>
                 <span className='font-semibold'>{number}</span>
 
             </div>
         </div>
-        <input type="date" className='w-5 h-5 absolute bottom-1 right-1'/>
+        <input type="date" className='w-5 h-5 float-right'/>
     </div>
     </>
   )
--- client/src/components/Dashboard/DashCard.jsx
+++ client/src/components/Dashboard/DashCard.jsx
@@ -6,13 +6,9 @@ function DashCard({title, number}) {
         <div className="absolute rounded-md border bg-white h-8 w-8 -top-2 flex justify-center items-center">
             <img src={UserIcon} alt="" />
         </div>
-        <div className="grid grid-cols-2">
-        {/* <div className='flex flex-col opacity-0'>
-                <span>TEXT</span>
-                <span>001</span>
-            </div> */}
+        <div className="flex justify-end">
             <div className='flex flex-col gap-2 text-right'>
-                <span className='text-[10px]'>{title}</span>
+                <span className='text-[12px]'>{title}</span>
                 <span className='font-semibold'>{number}</span>
             </div>
         </div>
