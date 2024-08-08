c981042909653a807162ce920d9db03b8a25107e
Ashutosh Jaiswal <amanashutoshjaiswal@gmail.com>
2024-08-08 15:38:02 +0530

    08-08-2024

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -37,31 +37,37 @@ const Dashboard = () => {
         34243, 31050, 33099, 33473,
       ],
     }
-  ] 
+  ];
+
+  const dashCardsData = [
+    { heading: "Total Users", value: "1000" },
+    { heading: "Active Users", value: "750" },
+    { heading: "New Users", value: "250" },
+    { heading: "Revenue", value: "$10,000" },
+    { heading: "Expenses", value: "$5,000" },
+    { heading: "Profits", value: "$5,000" },
+    { heading: "Customer Support", value: "95%" },
+    { heading: "Development", value: "90%" },
+    { heading: "Sales", value: "85%" },
+    { heading: "Marketing", value: "80%" },
+    { heading: "Feedback", value: "Positive" },
+    { heading: "Projects", value: "15" },
+    { heading: "Tasks", value: "30" },
+    { heading: "Completed Tasks", value: "25" },
+    { heading: "Pending Tasks", value: "5" },
+    { heading: "Growth", value: "20%" },
+  ];
 
   return (
     <>
       <NavBar />
       <div className="w-full flex">
-        <Sidebar/>
+        <Sidebar />
         <div className="flex flex-col w-full">
           <div className="grid grid-cols-8 gap-2 p-2 pt-4 w-full">
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
-            <DashCard />
+            {dashCardsData.map((card, index) => (
+              <DashCard key={index} heading={card.heading} value={card.value} />
+            ))}
           </div>
           <div className="grid grid-cols-2 w-full p-2">
             <PieChart
@@ -80,7 +86,14 @@ const Dashboard = () => {
             />
           </div>
           <div className="w-full p-2">
-            <LineChart containerId={"lineChartContainer"} title={"Images V/s Videos"} subtitle={"Images and videos uploaded by users"} seriesData={LineChartData} xAxisDescription={"Images"} yAxisTitle={"Videos"}/>
+            <LineChart
+              containerId={"lineChartContainer"}
+              title={"Images V/s Videos"}
+              subtitle={"Images and videos uploaded by users"}
+              seriesData={LineChartData}
+              xAxisDescription={"Images"}
+              yAxisTitle={"Videos"}
+            />
           </div>
         </div>
       </div>
